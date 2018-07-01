be0f72ffc3a3238aa58920686a043360f0d47e45
Adam Jarzebak <adam@jarzeabak.eu>
2018-07-01 16:16:33 +0100

    clean up after release. WIP for adding tone

--- test_robot.py
+++ test_robot.py
@@ -1,8 +1,12 @@
-from asip_service.mirto_robot import MirtoRobot
+from mirto_asip_manager import MirtoRobot
 
-def test1():
-    mirto = MirtoRobot()
-    mirto.test_encoders(0.1, 7)
+enabled_services = {"encoders": [True, False]}
+# Run services test
+mirto = MirtoRobot(enabled_services)
+mirto.get_version_info()
+mirto.test_encoders(0.1, 2)
+mirto.test_motor(True, 3)
+mirto.test_ir_sensors(5, 0.2)
+# This will stop all threads and close ports
+mirto.terminate()
 
-
-test1()
