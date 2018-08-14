5a0c49afd64c79cca3b45167cc44df5ccfc6d6a8
Unknown <yvan_kablan@hotmail.co.uk>
2018-08-14 16:24:56 +0100

    some up dates

--- sketch.js
+++ sketch.js
@@ -10,13 +10,17 @@ var robots_on_map = [];
 var robot_obj;
 var  array_traffic_light;
 var  array_traffic_light2;
-var reported_ajax_error = false;
+var reported_ajax_error = false;  
+var traffic_lightcolor;
 var traffic_set_gui;
-
+var reported_ajax_call = false;
 function setup() {
+
+ 
 
   //createCanvas(w, h, [renderer])
   createCanvas(720, 470);
+ 
 
   // width and height of the canvas
   x = width / 3;
@@ -57,9 +61,8 @@ function setup() {
     setInterval(fetch_ajax_traffic_lights,100);
     document.getElementById("trafficLight").addEventListener("click", show_traffic_light);
     //fectching ajax for robot every 100 millisecond second 
+    // setTimeout(populate_select_tag(array_traffic_light), 1000);
     fetch_ajax_traffic_lights_gui();
-    // console.log(array_traffic_light);
-    // console.log('Data not receivded, please check connection.')
   });
 
   //array containing all the buildings
@@ -75,11 +78,12 @@ function fetch_ajax_traffic_lights(){
   //error handling
   .done(function (data, textStatus, jqXHR) {
       array_traffic_light = data;
-      // console.log(array_traffic_light);
-      //generate_traffic_lights(array_traffic_light);
-      //  show_traffic_light(array_traffic_light);
+      if (reported_ajax_call != true) {
+        // populate_select_tag(data);
+        console.log("populate_select_tag");
+        reported_ajax_call = true;
+      }
 
-      
   })
   .fail(function (jqXHR, textStatus, errorThrown) {
     // console.log(errorThrown);
@@ -123,6 +127,7 @@ function fetch_ajax_traffic_lights_gui(){
 
 
 function draw() {
+   
   
    //defining the color
    background(200);
@@ -156,7 +161,7 @@ function draw() {
    generateBuildings(buildings);
   
    //generate traffic light
-     if (array_traffic_light !== undefined){
+    if (array_traffic_light !== undefined){
       generate_traffic_lights(array_traffic_light);
     }
   
@@ -172,12 +177,13 @@ function draw() {
    ellipse(xaxis_5, yaxis_5, 60,60);
 
 
- //console.log(traffic_set_gui);
- traffic_set_gui = color(204, 102, 0);
- generate_traffic_gui(traffic_set_gui);
+  //console.log(traffic_set_gui);
+  //set traffic light gui
+  /// traffic light color
+   traffic_set_gui = color(204, 102, 0);
+  generate_traffic_gui(traffic_set_gui);
   
-  }
-  
+ }
 
  // vertical dash line
  function vert_dash_line(x,start_pointY ,end_pointY) {
@@ -202,18 +208,18 @@ function draw() {
  }
 
  //controlling robot with arrow keys
- function keyPressed() {
-  if(keyCode == UP_ARROW) {
-    move_robot(robots[0],"up");
-  } else if (keyCode == DOWN_ARROW) {
-    move_robot(robots[0],"down");
-  } else if (keyCode == LEFT_ARROW) {
-    move_robot(robots[0],"left");
-  }else if (keyCode = RIGHT_ARROW){
-    move_robot(robots[0],"right");
-  }
-  return 0;
-}
+//  function keyPressed() {
+//   if(keyCode == UP_ARROW) {
+//     move_robot(robots[0],"up");
+//   } else if (keyCode == DOWN_ARROW) {
+//     move_robot(robots[0],"down");
+//   } else if (keyCode == LEFT_ARROW) {
+//     move_robot(robots[0],"left");
+//   }else if (keyCode = RIGHT_ARROW){
+//     move_robot(robots[0],"right");
+//   }
+//   return 0;
+// }
 
 //generate buildings
 function generateBuildings(recs){
@@ -224,7 +230,6 @@ function generateBuildings(recs){
   });
 
 }
-
 
 //generate robots
 function generate_robot(objs){
@@ -249,65 +254,65 @@ function generate_traffic_gui(traffic_gui_color){
 
 
 //write a function that take a robot object and takes a direction
-function move_robot(robot,direction,){
-  var valid_move = true;
-  //loop to go throught the building array
-  buildings.forEach(function(building){
-    switch (direction){
-      case "up":
-        if(robot.x > building.x && robot.x < (building.x + building.width)) {
-          if ((robot.y - (robot.height / 2) - step) > building.y && (robot.y - (robot.height / 2) - step) < (building.y + building.height)){
-                valid_move = false;
-          }
-        }
-        break;  
-      case "down":
-        if(robot.x > building.x && robot.x < (building.x + building.width)) {
-          if ((robot.y + (robot.height / 2) + step) > building.y && ((robot.y + (robot.height / 2) + step) < (building.y + building.height))){
-            valid_move = false;
-          }
-        }
-        break;
+// function move_robot(robot,direction,){
+//   var valid_move = true;
+//   //loop to go throught the building array
+//   buildings.forEach(function(building){
+//     switch (direction){
+//       case "up":
+//         if(robot.x > building.x && robot.x < (building.x + building.width)) {
+//           if ((robot.y - (robot.height / 2) - step) > building.y && (robot.y - (robot.height / 2) - step) < (building.y + building.height)){
+//                 valid_move = false;
+//           }
+//         }
+//         break;  
+//       case "down":
+//         if(robot.x > building.x && robot.x < (building.x + building.width)) {
+//           if ((robot.y + (robot.height / 2) + step) > building.y && ((robot.y + (robot.height / 2) + step) < (building.y + building.height))){
+//             valid_move = false;
+//           }
+//         }
+//         break;
   
-      case "left":
-      if(robot.y > building.y && robot.y < (building.y + building.height)) {
-        if ((robot.x - (robot.width / 2) - step) > building.x && ((robot.x - (robot.width / 2) - step) < (building.x + building.width))){
-          valid_move = false;
-        }
-      }
-        break;
-      case "right":
-      if(robot.y > building.y && robot.y < (building.y + building.height)) {
-        if ((robot.x + (robot.width / 2) + step) > building.x && ((robot.x + (robot.width / 2) + step) < (building.x + building.width))){
-          valid_move = false;
-        }
-      }
-        break;
-    }
-  });
+//       case "left":
+//       if(robot.y > building.y && robot.y < (building.y + building.height)) {
+//         if ((robot.x - (robot.width / 2) - step) > building.x && ((robot.x - (robot.width / 2) - step) < (building.x + building.width))){
+//           valid_move = false;
+//         }
+//       }
+//         break;
+//       case "right":
+//       if(robot.y > building.y && robot.y < (building.y + building.height)) {
+//         if ((robot.x + (robot.width / 2) + step) > building.x && ((robot.x + (robot.width / 2) + step) < (building.x + building.width))){
+//           valid_move = false;
+//         }
+//       }
+//         break;
+//     }
+//   });
 
-  if (valid_move) {
-    switch (direction){
-      case "up":
-        robot.y = robot.y - step;
-        break;
+//   if (valid_move) {
+//     switch (direction){
+//       case "up":
+//         robot.y = robot.y - step;
+//         break;
 
-      case "down":
-        robot.y = robot.y + step;
-        break;
+//       case "down":
+//         robot.y = robot.y + step;
+//         break;
   
-      case "left":
-        robot.x = robot.x - step;
-        break;
+//       case "left":
+//         robot.x = robot.x - step;
+//         break;
   
-      case "right":
-        robot.x = robot.x + step;
-        break;
+//       case "right":
+//         robot.x = robot.x + step;
+//         break;
         
-    }
-  }
+//     }
+//   }
 
-}
+// }
 
 
 //one fucntion that check the name of the array
@@ -333,9 +338,6 @@ function comparer(otherArray){
     }).length == 0;
   }
 }
-
-
-
 //generate traffic lights
 function  generate_traffic_lights(lights){
   lights = lights[0];
@@ -355,11 +357,13 @@ function  generate_traffic_lights(lights){
 
 // populating select tag
  function  populate_select_tag(lights2){
-
   lights2 = lights2[0];
+  console.log("ttt", lights2);
+  var x = document.getElementById("trafficLight");
+  
+
+
   for (var t in lights2) {
-    lights2 = lights2[t];
-    var x = document.getElementById("trafficLight");
     var option = document.createElement("option");
     option.text = t;
     x.add(option);
@@ -367,34 +371,34 @@ function  generate_traffic_lights(lights){
 
 }
 
-
 function show_traffic_light(){
+  
   var trafficaValue = document.getElementById('trafficLight').value;
   // console.log("light3 " + trafficaValue);
   // console.log(array_traffic_light);
-
   // compare the json data coming through
   lights_to_display = array_traffic_light[0];
   for (var traffic_light_object in lights_to_display) {
     traffic_light = lights_to_display[traffic_light_object];
     // console.log(traffic_light.status);
-    console.log(traffic_light_object);
+    //console.log(traffic_light_object);
     //name match
     if(traffic_light_object == trafficaValue){
+      // console.log(traffic_light.status);
         //  matching for the color corresponding 
-         if (traffic_light.status == green){
+         if (traffic_light.status == "green"){
+              console.log(" change to green");
               //set color to green
-              traffic_set_gui = color(4, 251, 0);
-              generate_traffic_gui(traffic_set_gui);
+              // traffic_set_gui = color(4, 251, 0);
           }
-    }else if (traffic_light.status == red){
+    }else if (traffic_light.status == "red"){
+              console.log("change to red");
               //change color to red
-              traffic_set_gui = color(253, 0, 0);
-              generate_traffic_gui(traffic_set_gui);
+              // traffic_set_gui = color(253, 0, 0);
     }else {
        //change color to red
-       traffic_set_gui = color(253, 254, 254);
-       generate_traffic_gui(traffic_set_gui);
+      console.log(" change to black");
+      //  traffic_set_gui = color(253, 254, 254);
       
     }
   };
